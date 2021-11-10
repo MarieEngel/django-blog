9f729c3b64a716822635640c87dc5bfa436d9562
Marie Engel <marie.engel@deliveryhero.com>
2021-11-10 10:22:43 +0100

    added update func

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,9 @@
+import git
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from .models import Blog
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -14,3 +16,21 @@ def blog_post(request, id=1):
     blog = Blog.objects.get(id=id)
     context = {"blog": blog}
     return render(request, "blog/blog_post.html", context)
+
+
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        """
+        pass the path of the directory where your project will be
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "raphaelavergud.pythonanywhere.com/"
+        """
+        repo = git.Repo("django_blog/")
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
