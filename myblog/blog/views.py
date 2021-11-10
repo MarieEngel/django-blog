2d49894153654f950ba1d33ce226746017f3735f
Marie Engel <marie.engel@deliveryhero.com>
2021-11-10 11:27:06 +0100

    corrected path

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -26,7 +26,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "raphaelavergud.pythonanywhere.com/"
         """
-        repo = git.Repo("django_blog/")
+        repo = git.Repo("myblog/")
         origin = repo.remotes.origin
 
         origin.pull()
