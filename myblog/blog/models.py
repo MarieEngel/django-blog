1dd4cc66c483d41ca4474785bc7eed0f4c20e77e
Marie Engel <marie.engel@deliveryhero.com>
2021-11-11 15:26:25 +0100

    added field 'title_tag'

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=150)
+    title_tag = models.CharField(max_length=150, default="Title")
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     body = models.TextField()
     date = models.DateTimeField(auto_now=True)
