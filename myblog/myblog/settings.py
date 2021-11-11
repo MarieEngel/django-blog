24e22a1349632a153cd6b19cd5555db39e960cdc
Marie Engel <marie.engel@deliveryhero.com>
2021-11-11 09:54:04 +0100

    moved import local_settings to very bottom

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -167,13 +167,15 @@ MARKDOWNIFY = {
         ],
     }
 }
-try:
-    from local_settings import *
-except ImportError:
-    pass
+
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
