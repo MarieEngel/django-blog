ccc9a745003b3b63ffb8ef516dfafbb18b8fcb7d
Marie Engel <marie.engel@deliveryhero.com>
2021-11-12 12:43:18 +0100

    function to return 'home' after posting

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Blog(models.Model):
@@ -10,6 +11,9 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("home")
 
 
 # class Image(models.Model):
