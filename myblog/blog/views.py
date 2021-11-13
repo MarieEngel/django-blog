0a83372e70ac0b49f36414b4e2beebe3cd6667bc
Marie Engel <marie.engel@deliveryhero.com>
2021-11-13 22:06:00 +0100

    deleted comment, updated  contact func & send_mail

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -25,32 +25,27 @@ class AddPostView(CreateView):
     model = Blog
     form_class = BlogForm
     template_name = "blog/add_blogpost.html"
-    # fields = "__all__"  # fields and form_class can not be specified both
-
-
-# class ContactView(CreateView):
-#     model = Blog
-#     template_name = "blog/contact.html"
 
 
 def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = "Website Inquiry"
-            body = {
-                "first_name": form.cleaned_data["first_name"],
-                "last_name": form.cleaned_data["last_name"],
-                "email": form.cleaned_data["email_address"],
-                "message": form.cleaned_data["message"],
-            }
-            message = "\n".join(body.values())
-
+            cleaned = form.cleaned_data
+            message = f"""
+            from: {cleaned["name"]} {cleaned["email"]}
+            {cleaned["message"]}
+            """
             try:
-                send_mail(subject, message, "admin@example.com", ["admin@example.com"])
+                send_mail(
+                    cleaned["subject"],
+                    message,
+                    "no-other-addresses-were-there@outlook.com",
+                    ["no-other-addresses-were-there@outlook.com"],
+                )
             except BadHeaderError:
                 return HttpResponse("Invalid header found.")
-            return redirect("main:homepage")
+            return redirect("home")
 
     form = ContactForm()
     return render(request, "blog/contact.html", {"form": form})
