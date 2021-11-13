98f05d14935c212c601f1232be49f8aa93da9379
Marie Engel <marie.engel@deliveryhero.com>
2021-11-13 22:00:34 +0100

    fixed typo, removed comments

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -16,7 +16,7 @@ class BlogForm(forms.ModelForm):
 
 
 class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=150)
+    name = forms.CharField(max_length=50)
+    subject = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=150)
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
