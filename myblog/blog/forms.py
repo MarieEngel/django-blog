20ad42d074881fc74dffac247fed3e391ed77a1b
Marie Engel <marie.engel@deliveryhero.com>
2021-12-06 13:17:47 +0100

    added required for fields email & message

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -16,5 +16,8 @@ class BlogForm(forms.ModelForm):
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=50)
     subject = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=150)
-    message = forms.CharField(widget=forms.Textarea, max_length=2000)
+    email = forms.EmailField(
+        max_length=150,
+        required=True,
+    )
+    message = forms.CharField(widget=forms.Textarea, max_length=2000, required=True)
