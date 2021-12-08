6c5273e176d526e6309444e7021c9ca878eaf6b9
Marie Engel <marie.engel@deliveryhero.com>
2021-12-08 09:55:03 +0100

    fix create user for login test with selenium

--- myblog/blog/tests/test_login.py
+++ myblog/blog/tests/test_login.py
@@ -1,5 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+import time
+from django.contrib.auth.models import User
 
 
 class MySeleniumTests(StaticLiveServerTestCase):
@@ -10,6 +12,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
+        cls.user = User.objects.create_user(username="testuser1", password="password")
 
     @classmethod
     def tearDownClass(cls):
@@ -17,9 +20,12 @@ class MySeleniumTests(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_login(self):
+        "Test login with selenium."
+        self.client.login(username="testuser1", password="password")
         self.selenium.get("%s%s" % (self.live_server_url, "/members/login/"))
         username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys("myuser")
+        username_input.send_keys("testuser1")
         password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys("secret")
+        password_input.send_keys("password")
         self.selenium.find_element_by_id("login-button").click()
+        time.sleep(100)
